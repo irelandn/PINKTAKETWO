f1aea8720ab7cd126319ed676f6d159f0ad0f939
irelandn <irelandn@student.unimelb.edu.au>
2021-08-21 16:40:33 +1000

    Working

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ const app = express();
 
 require('./models/db');
 
-// //set up user route
-// userRouter = require('./routes/userRouter')
+const taxReceiptRouter = require('./routes/taxReceiptRouter')
 
-app.get('/', (req, res) => {
+// simple route
+app.get("/", (req, res) => {
     res.send('<h1>IT Project 2021 Semester 2 Capstone</h1>')
-})
-
-// //Handle user requests
-// app.use('/info', userRouter)
-
+  });
+  
+//Handle user requests
+app.use('/tax-receipt', taxReceiptRouter)
 
 
 app.listen(3000, () => {
-    console.log('The CRM app is listening on port 3000!')
+    console.log('The pink app is listening on port 3000!')
 })
