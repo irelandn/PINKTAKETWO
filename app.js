11c84a828ffa344110ea1dbd8ed473cea0bcd0fe
Koosha-Owji <koosha.owji@yahoo.com>
2021-08-21 20:07:27 +1000

    user functionality

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+require('rootpath')();
+const cors = require('cors');
+const jwt = require('./models/jwt');
 const express = require('express')
 const app = express();
+const errorHandler = require('./models/error-handler');
 
 require('./models/db');
 
+
 const taxReceiptRouter = require('./routes/taxReceiptRouter')
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(jwt());
+app.use('/users', require('./routes/userRouter'));
+app.use(errorHandler);
 
 // simple route
 app.get("/", (req, res) => {
