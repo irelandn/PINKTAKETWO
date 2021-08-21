3208b0f402719e4d26da2265a3a1b467f0cd267a
irelandn <irelandn@student.unimelb.edu.au>
2021-08-21 16:34:03 +1000

    Connected to db

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const app = express();
 
 require('./models/db');
 
-//set up user route
-userRouter = require('./routes/userRouter')
+// //set up user route
+// userRouter = require('./routes/userRouter')
 
 app.get('/', (req, res) => {
     res.send('<h1>IT Project 2021 Semester 2 Capstone</h1>')
 })
 
-//Handle user requests
-app.use('/info', userRouter)
+// //Handle user requests
+// app.use('/info', userRouter)
 
 
 
